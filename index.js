8234f8461afef41218b69abfd9a6264a3d94f6f1
Eng Ibrahim Reda <devspectorx@gmail.com>
2026-09-24 23:34:32 +0300

    Update bot phone number in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '201038436911', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
